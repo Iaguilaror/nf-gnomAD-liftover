56c9dae069dc6308aff0032d32d1bb094e8bdc9e
Israel Aguilar <iaguilar@inmegen.edu.mx>
2019-03-24 22:15:28 +0000

    changes at inmegen first run

---
+++ launchers/genomes_at_warwick.sh
@@ -0,0 +1,10 @@
+#!/usr/local/env bash
+
+nextflow run liftover.nf \
+	--vcf_dir real-data/genomes/ \
+	--genome_fasta /home/iaguilar/Ongoing_projects/lifting_over_gnomAD/nf-gnomAD-liftover/real-data/reference/Homo_sapiens_assembly38.fa \
+	--chainfile test/reference/chainfile/GRCh37_to_GRCh38.chain \
+	--output_dir real-data/genomes/results/ \
+	-resume \
+	-with-report real-data/genomes/results/logs/`date +%Y%m%d_%H%M%S`_report.html \
+	-with-dag real-data/genomes/results/logs/`date +%Y%m%d_%H%M%S`.DAG.html
--- runtest.sh
+++ runtest.sh
@@ -9,7 +9,7 @@ echo -e "======\n Testing NF execution \n======" \
 && rm -rf test/results/ \
 && nextflow run liftover.nf \
 	--vcf_dir test/data/ \
-	--genome_fasta /home/acerlap/Databases/genomes/hs_ref_GRCh38.p12.fa \
+	--genome_fasta /home/iaguilar/Ongoing_projects/lifting_over_gnomAD/nf-gnomAD-liftover/real-data/reference/Homo_sapiens_assembly38.fa \
 	--chainfile test/reference/chainfile/GRCh37_to_GRCh38.chain \
 	--output_dir test/results \
 	-resume \
