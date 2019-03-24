8fdc9f1de5c6b580fbacd4a97e011156e47d8725
Israel Aguilar <iaguilaror@gmail.com>
2019-03-24 16:35:45 -0600

    changed testdata path

--- runtest.sh
+++ runtest.sh
@@ -1,15 +1,10 @@
 #!/usr/local/env bash
 
-# NOT IMPLEMENTEDCheck that command requirements are reachable from CLI
-## then remove test/results dir
-## then Run NF
-# bash test/requirements/dependency_checker.sh \
-# && echo -e "======\n Testing NF execution \n======" \
 echo -e "======\n Testing NF execution \n======" \
 && rm -rf test/results/ \
 && nextflow run liftover.nf \
 	--vcf_dir test/data/ \
-	--genome_fasta /home/iaguilar/Ongoing_projects/lifting_over_gnomAD/nf-gnomAD-liftover/real-data/reference/Homo_sapiens_assembly38.fa \
+	--genome_fasta /home/acerlap/Databases/genomes/hs_ref_GRCh38.p12.fa \
 	--chainfile test/reference/chainfile/GRCh37_to_GRCh38.chain \
 	--output_dir test/results \
 	-resume \
