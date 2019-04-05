1478766e45163b96cf5cbe84ab36c87feae5d3a0
Israel Aguilar <iaguilar@inmegen.edu.mx>
2019-04-05 00:55:37 +0000

    functional version at inmegen

--- launchers/exomes_at_warwick.sh
+++ launchers/exomes_at_warwick.sh
@@ -4,7 +4,10 @@ nextflow run liftover.nf \
 	--vcf_dir real-data/exomes/ \
 	--genome_fasta /home/iaguilar/Ongoing_projects/lifting_over_gnomAD/nf-gnomAD-liftover/real-data/reference/Homo_sapiens_assembly38.fa \
 	--chainfile test/reference/chainfile/GRCh37_to_GRCh38.chain \
-	--output_dir real-data/genomes/results/ \
+	--chunks 10 \
+	--rehead false \
+	--filter_PASS false \
+	--output_dir real-data/exomes/results/ \
 	-resume \
-	-with-report real-data/genomes/results/logs/`date +%Y%m%d_%H%M%S`_report.html \
-	-with-dag real-data/genomes/results/logs/`date +%Y%m%d_%H%M%S`.DAG.html
+	-with-report real-data/exomes/results/logs/`date +%Y%m%d_%H%M%S`_report.html \
+	-with-dag real-data/exomes/results/logs/`date +%Y%m%d_%H%M%S`.DAG.html
--- launchers/genomes_at_warwick.sh
+++ launchers/genomes_at_warwick.sh
@@ -4,6 +4,9 @@ nextflow run liftover.nf \
 	--vcf_dir real-data/genomes/ \
 	--genome_fasta /home/iaguilar/Ongoing_projects/lifting_over_gnomAD/nf-gnomAD-liftover/real-data/reference/Homo_sapiens_assembly38.fa \
 	--chainfile test/reference/chainfile/GRCh37_to_GRCh38.chain \
+	--chunks 100 \
+	--rehead false \
+	--filter_PASS false \
 	--output_dir real-data/genomes/results/ \
 	-resume \
 	-with-report real-data/genomes/results/logs/`date +%Y%m%d_%H%M%S`_report.html \
